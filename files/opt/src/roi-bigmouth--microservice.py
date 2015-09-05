f9aacd711711e80ad7d89d1fd9112810d5f1901b
Andreu Belmonte <andreubelmonte@bargain.local>
2015-09-05 16:57:56 +0200

    fix encoding

--- files/opt/src/roi-bigmouth--microservice.py
+++ files/opt/src/roi-bigmouth--microservice.py
@@ -2,13 +2,16 @@ from flask import Flask, jsonify, render_template
 from gtts import gTTS
 import requests
 import uuid
+import traceback
 
 app = Flask(__name__)
 
 MP3 = dict()
+debug=False
 
 def generate_mp3(quote):
-   url = uuid.uuid5(uuid.NAMESPACE_DNS,quote)
+   url = uuid.uuid5(uuid.NAMESPACE_DNS,str(quote))
+   if debug: print url
    if not url in MP3:
        tts = gTTS(text=quote, lang='en')
        tts.save("/tmp/{}".format(url))
@@ -22,12 +25,15 @@ def roi_mp3(file):
 @app.route('/')
 def roi_speak():
    try:
-        r = requests.get("http://localhost:8078/proxy/?vip=team12&path=/api/quote/random")
+        #r = requests.get("http://localhost:8078/proxy/?vip=team12&path=/api/quote/random")
+        r = requests.get("http://ec2-52-19-172-174.eu-west-1.compute.amazonaws.com:5000/api/quote/random")
+        if debug: print r
         q = r.json()["quote"]
+        if debug: print q
         #q = "Winter is coming"
         return render_template('index.html', quote=q, mp3_url=generate_mp3(q))
    except Exception as error:
-        return not_found('Quote service error. {}'.format(error))
+        return not_found('Quote service error. {}'.format(traceback.format_exc()))
 
 @app.route('/healthcheck')
 def healthcheck():
